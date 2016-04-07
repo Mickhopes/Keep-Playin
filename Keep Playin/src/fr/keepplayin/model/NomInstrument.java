c37fa0909b41abb50d738a1076d42be793bc7195
Mickhopes <mickael.turnel@gmail.com>
2016-04-07 16:21:12 +0200

    Ajout des noms

--- Keep Playin/src/fr/keepplayin/model/NomInstrument.java
+++ Keep Playin/src/fr/keepplayin/model/NomInstrument.java
@@ -1,19 +1,31 @@
 package fr.keepplayin.model ;
 
 public enum NomInstrument {
-    GUITARESECHE,
-    GUITAREELECTRIQUE,
-    ALTO,
-    VIOLON,
-    VIOLONCELLE,
-    CONTREBASSE,
-    PIANO,
-    HARPE,
-    BATTERIE,
-    THEREMINE,
-    SYNTHETISEUR,
-    FLUTE,
-    FLUTEABEC,
-    FLUTETRAVERSIERE,
-    FLUTEDEPAN
+    GUITARESECHE("Guitare sèche"),
+    GUITAREELECTRIQUE("Guitare électrique"),
+    BASSE("Basse"),
+    ALTO("Alto"),
+    VIOLON("Violon"),
+    VIOLONCELLE("Violoncelle"),
+    CONTREBASSE("Contrebasse"),
+    PIANO("Piano"),
+    HARPE("Harpe"),
+    BATTERIE("Batterie"),
+    THEREMINE("Theremine"),
+    SYNTHETISEUR("Synthétiseur"),
+    FLUTE("Flûte"),
+    FLUTEABEC("Flûte à bec"),
+    FLUTETRAVERSIERE("Flûte traversière"),
+    FLUTEDEPAN("Flûte de pan");
+    
+    private String nom;
+    
+    NomInstrument(String nom) {
+    	this.nom = nom;
+    }
+    
+    @Override
+    public String toString() {
+    	return nom;
+    }
 }
