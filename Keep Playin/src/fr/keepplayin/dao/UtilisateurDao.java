312edb1a25e1316d65dfb444483803cce9316a31
Mickhopes <mickael.turnel@gmail.com>
2016-04-12 17:24:58 +0200

    Ajout de recherche d'utilisateur

--- Keep Playin/src/fr/keepplayin/dao/UtilisateurDao.java
+++ Keep Playin/src/fr/keepplayin/dao/UtilisateurDao.java
@@ -1,5 +1,6 @@
 package fr.keepplayin.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.keepplayin.model.Utilisateur;
@@ -16,5 +17,18 @@ public class UtilisateurDao extends BaseDao<Utilisateur>{
 	
 	public List<Utilisateur> chercherUtilisateur(String field, String value) {
 		return query().filter(field + " >=", value).filter(field + " <", value + "\uFFFD").list();
+	}
+	
+	public List<Utilisateur> chercherUtilisateur(String recherche) {
+		List<Utilisateur> uList = new ArrayList<Utilisateur>();
+		List<Utilisateur> all = super.getAll();
+		
+		for(Utilisateur u : all) {
+			if ((u.getPrenom() + " " + u.getNom()).toLowerCase().startsWith(recherche.toLowerCase())) {
+				uList.add(u);
+			}
+		}
+		
+		return uList;
 	}
 }
